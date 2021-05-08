307a8fcda631d2ef7107ef7fd2f9e3b3422645e6
Bala Venkataraman <38150148+balavenkataraman123@users.noreply.github.com>
2021-05-08 18:34:57 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,53 @@
+import numpy as np
+import cv2
+import pyvirtualcam
+import keyboard
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+coolshades = []
+imageparameters = []
+for i in range(8):
+    x = (i+1)*112
+    y = int(x/3)
+    itemp = []
+    itemp.append(int(x/10))
+    itemp.append(0 - int(x/14))
+    imageparameters.append(itemp)
+    coolshades.append(cv2.resize(cv2.imread('sunglasses.png', -1), (x, y), interpolation = cv2.INTER_AREA))
+activate = 0
+faces = [[100, 100, 100, 100]]
+cooldown = 0
+cooldown1 = 0
+with pyvirtualcam.Camera(width=1920, height=1080, fps=30) as cam:
+    i = 0
+    while(True):
+        if cooldown == 0 and keyboard.is_pressed ('f8'): 
+            activate = (activate + 1) % 2
+            cooldown = 5
+            print(['deactivated','activated'][activate])
+        i += 1
+        ret, frame = cap.read()
+        if activate == 1:
+            frame1 = cv2.resize(frame, (480, 270), interpolation = cv2.INTER_AREA)
+            gray = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+            if i % 10 == 0:
+                faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+            try:
+                for (x,y,w,h) in faces:
+                    shadenum = int(round(w/25) - 1)
+                    y1, y2 = y*4 + imageparameters[shadenum][0], y*4 + imageparameters[shadenum][0] + coolshades[shadenum].shape[0]
+                    x1, x2 = x*4 + imageparameters[shadenum][1], x*4 + imageparameters[shadenum][1] + coolshades[shadenum].shape[1]
+                    alpha_s = coolshades[shadenum][:, :, 3] / 255.0
+                    alpha_l = 1.0 - alpha_s
+                    for c in range(0, 3):
+                        frame[y1:y2, x1:x2, c] = (alpha_s * coolshades[shadenum][:, :, c] + 
+                                                alpha_l * frame[y1:y2, x1:x2, c])
+            except ValueError:
+                pass
+        cam.send(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        cooldown = max(cooldown - 1, 0)
+        cooldown1 = max(cooldown1 - 1, 0)
+cap.release()
+cv2.destroyAllWindows()
