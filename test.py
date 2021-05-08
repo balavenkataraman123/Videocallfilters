0ba4665e628068647e01109062e8a1867bf31fa9
Bala Venkataraman <38150148+balavenkataraman123@users.noreply.github.com>
2021-05-08 18:56:38 -0400

    Update test.py

--- test.py
+++ test.py
@@ -4,12 +4,17 @@ import pyvirtualcam
 import keyboard
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+camwidth = 1920
+camheight = 1080
+
+camscale = 1 #divide your resolution by 1920 1080
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, camwidth)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, camheight)
 coolshades = []
 imageparameters = []
 for i in range(8):
-    x = (i+1)*112
+    x = int((i+1)*112*camscale)
     y = int(x/3)
     itemp = []
     itemp.append(int(x/10))
@@ -20,7 +25,7 @@ activate = 0
 faces = [[100, 100, 100, 100]]
 cooldown = 0
 cooldown1 = 0
-with pyvirtualcam.Camera(width=1920, height=1080, fps=30) as cam:
+with pyvirtualcam.Camera(width=camwidth, height=camheight, fps=30) as cam:
     i = 0
     while(True):
         if cooldown == 0 and keyboard.is_pressed ('f8'): 
@@ -30,13 +35,13 @@ with pyvirtualcam.Camera(width=1920, height=1080, fps=30) as cam:
         i += 1
         ret, frame = cap.read()
         if activate == 1:
-            frame1 = cv2.resize(frame, (480, 270), interpolation = cv2.INTER_AREA)
+            frame1 = cv2.resize(frame, (int(480 * camscale), int(270* camscale)), interpolation = cv2.INTER_AREA)
             gray = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
             if i % 10 == 0:
                 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
             try:
                 for (x,y,w,h) in faces:
-                    shadenum = int(round(w/25) - 1)
+                    shadenum = int(round(w/int(25 * camscale)) - 1)
                     y1, y2 = y*4 + imageparameters[shadenum][0], y*4 + imageparameters[shadenum][0] + coolshades[shadenum].shape[0]
                     x1, x2 = x*4 + imageparameters[shadenum][1], x*4 + imageparameters[shadenum][1] + coolshades[shadenum].shape[1]
                     alpha_s = coolshades[shadenum][:, :, 3] / 255.0
